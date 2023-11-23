d2c52ca7699b1ecdfe4b0533e666889eb027c5e8
ksnvmm <abduxakimovamubina0@gmail.com>
2023-11-23 15:49:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+let theme = false
+
+let body = document.querySelector("body")  
+let button = document.querySelector("button")  
+let nav = document.querySelector("nav")  
+// input.style.background = "transparent"  
+  
+let cards = document.querySelectorAll('.flags-card')
+
+console.log(cards)
+
+cards[1].addEventListener('click' , function(){
+    window.location="http://127.0.0.1:5500/germany.html"
+})
+
+cards[0].addEventListener('click' , function(){
+    window.location="http://127.0.0.1:5500/germany.html"
+})
+
+cards[0].addEventListener('click' , function(){
+    window.location="http://127.0.0.1:5500/germany.html"
+})
+
+cards[0].addEventListener('click' , function(){
+    window.location="http://127.0.0.1:5500/germany.html"
+})
+
+
+// button.addEventListener("click", function () {  
+//     if (theme === false) {  
+//         theme = true  
+//     } else {  
+//         theme = false  
+//     }  
+//     console.log(theme)  
+  
+//     if (theme === false) {  
+//         body.style.background = "white"  
+//         body.style.color = "black"  
+//     } else {  
+//         body.style.background = "black"  
+//         body.style.color = "white"  
+//     }  
+// })
